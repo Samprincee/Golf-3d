9d4c666f429952fd9bb19fceb9952597935646e5
Prince Kumar <128685487+Samprincee@users.noreply.github.com>
2024-10-11 14:56:08 +0530

    complete it with help

--- script.js
+++ script.js
@@ -1,44 +1,128 @@
 var crsr = document.querySelector("#cursor");
-var blur = document.querySelector("#cursor-blur");
+var dhun = document.querySelector("#cursor-blur");
 
-document.addEventListener("mousemove", function(detail){
+document.addEventListener("mousemove", function (detail) {
     crsr.style.left = detail.x + "px";
     crsr.style.top = detail.y + "px";
 
-    blur.style.left = detail.x -200 + "px";
-    blur.style.top = detail.y -200 + "px";
-    
-    
+    dhun.style.left = detail.x - 200 + "px";
+    dhun.style.top = detail.y - 200 + "px";
+
+
 })
 
-gsap.to("#nav", {
-    backgroundColor:"#000",
-    duration:0.5,
-    height:"120px",
+var h4all = document.querySelectorAll("#nav h4")
 
-    scrollTrigger:{
-        trigger:"#nav",
-        scroller:"body",
-        markers:true,
+h4all.forEach(function (elem) {
+    elem.addEventListener("mouseenter", ()=>{
+        crsr.style.scale=3
+        crsr.style.border= " 1px solid #fff"
+        crsr.style.backgroundColor = "transparent"
+    })
+    elem.addEventListener("mouseleave", ()=>{
+        crsr.style.scale=1
+        crsr.style.border= " 0px solid #fff"
+        crsr.style.backgroundColor = "#95C11E"
+    })
+})
+
+
+gsap.to("#nav", {
+    backgroundColor: "#000",
+    duration: 0.5,
+    height: "120px",
+
+    scrollTrigger: {
+        trigger: "#nav",
+        scroller: "body",
+        // markers: true,
         start: "top -10%",
         end: "top -11%",
-        scrub:2
+        scrub: 2
     }
 
 })
 
 gsap.to("#main", {
-    backgroundColor:"#000",
-    duration:0.5,
+    backgroundColor: "#000",
+    duration: 0.5,
 
-    scrollTrigger:{
-        trigger:"#main",
-        scroller:"body",
-        markers:true,
+    scrollTrigger: {
+        trigger: "#main",
+        scroller: "body",
+        // markers: true,
         start: "top -45%",
         end: "top -100%",
-        scrub:1
+        scrub: 1
     }
 
 })
 
+gsap.from("#about-us img,#about-us-in",{
+    y:90,
+    opacity:0,
+    duration:1,
+
+    scrollTrigger:{
+        trigger:'#about-us',
+        scroller:"body",
+        // markers:true,
+        start:"top 60%",
+        end:"top 55%",
+        scrub:5
+    }
+})
+
+gsap.from(".card",{
+    scale:0.8,
+    opacity:0,
+    duration:1,
+
+    scrollTrigger:{
+        trigger:'.card',
+        scroller:"body",
+        // markers:true,
+        start:"top 60%",
+        end:"top 55%",
+        scrub:2
+    }
+})
+
+gsap.from("#colon1",{
+    y:-70,
+    x:-70,
+
+    scrollTrigger:{
+        trigger:'#page3',
+        scroller:"body",
+        // markers:true,
+        start:"top 55%",
+        end:"top 45%",
+        scrub:5
+    }
+})
+gsap.from("#colon2",{
+    y:70,
+    x:70,
+
+    scrollTrigger:{
+        trigger:'#page3',
+        scroller:"body",
+        // markers:true,
+        start:"top 55%",
+        end:"top 45%",
+        scrub:5
+    }
+})
+
+gsap.from("#page4 h1", {
+    y: 50,
+    scrollTrigger: {
+      trigger: "#page4 h1",
+      scroller: "body",
+    //   markers:true,
+      start: "top 75%",
+      end: "top 70%",
+      scrub: 3,
+    },
+  });
